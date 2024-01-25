1ac3827bdb4f749f94249f82536fdfd835588615
Rohit Singh <rohitbsb08@outlook.com>
2024-01-25 18:56:07 +0530

    Web Development Asignment on Product Managanemment

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Ajax.Utilities;
+using ProductManagement.Models;
+using ProductManagement.Repository;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository productRepository;
+        private readonly string connectionString;
+        
+        public ProductController()
+        {
+            connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProductMgmt;Integrated Security=True;";
+            productRepository = new ProductRepository(connectionString);
+        }
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = productRepository.GetProducts();
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    productRepository.AddProduct(product);
+                    return RedirectToAction("Index");
+                }
+
+                return View(product);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Error occurred while adding a new product.";
+                return View("Error");
+            }
+        }
+
+        // GET: Products/Delete/1
+        public ActionResult Delete(int id)
+        {
+            var product = productRepository.GetProductById(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/1
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            productRepository.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
+
+
+
+        // GET: Products/Edit/1
+        public ActionResult Edit(int id)
+        {
+            var product = productRepository.GetProductById(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+
+
+        // POST: Products/Edit/1
+        [HttpPost, ActionName("Edit")]
+        public ActionResult Edit(Product updatedProduct)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    productRepository.UpdateProduct(updatedProduct);
+                    return RedirectToAction("Index");
+                }
+
+                return View(updatedProduct);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it as appropriate for your application
+                ViewBag.ErrorMessage = "Error occurred while updating the product.";
+                return View("Error");
+            }
+        }
+
+    }
+}
---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,145 @@
+﻿using ProductManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProductManagement.Repository
+{
+    public class ProductRepository
+    {
+        private readonly string connectionString;
+
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Product";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Description = reader["Description"].ToString(),
+                                Price = Convert.ToDecimal(reader["Price"])
+                            };
+
+                            products.Add(product);
+                        }
+                    }
+                }
+            }
+
+            return products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Product (Name, Description, Price) VALUES (@Name, @Description, @Price)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", product.Name);
+                    command.Parameters.AddWithValue("@Description", product.Description);
+                    command.Parameters.AddWithValue("@Price", product.Price);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateProduct(Product updatedProduct)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Product SET Name = @Name, Description = @Description, Price = @Price WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", updatedProduct.Id);
+                    command.Parameters.AddWithValue("@Name", updatedProduct.Name);
+                    command.Parameters.AddWithValue("@Description", updatedProduct.Description);
+                    command.Parameters.AddWithValue("@Price", updatedProduct.Price);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Product WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Product GetProductById(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM Product WHERE Id = @Id";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                Product product = new Product
+                                {
+                                    Id = Convert.ToInt32(reader["Id"]),
+                                    Name = reader["Name"].ToString(),
+                                    Description = reader["Description"].ToString(),
+                                    Price = Convert.ToDecimal(reader["Price"])
+                                };
+
+                                return product;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error occurred while fetching product by ID.", ex);
+            }
+
+            return null; 
+        }
+
+    }
+
+}
